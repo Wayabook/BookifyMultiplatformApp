e0f41468abe1d44949eed2fe1b7b51038e4d31aa
rspaimoc2 <raduspaimoc@gmail.com>
2020-06-07 20:55:11 +0200

    [WIP] Carousel created from list.

--- lib/Pages/book_page.dart
+++ lib/Pages/book_page.dart
@@ -1,18 +1,20 @@
+import 'package:bookifyapp/Models/Book.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 class BookPage extends StatelessWidget {
   final String title;
+  final List<Book> books;
 
-  const BookPage(this.title) : assert(title != null);
+  const BookPage(this.title, this.books) : assert(title != null);
 
   @override
   Widget build(BuildContext context) {
     final text = Text('Details of $title');
     return Scaffold(
       body: Container(
-        child: ExampleHorizontal(),
+        child: ExampleHorizontal(books),
       ),
       appBar: AppBar(title: text),
     );
@@ -20,6 +22,10 @@ class BookPage extends StatelessWidget {
 }
 
 class ExampleHorizontal extends StatelessWidget {
+  final List<Book> books;
+
+  const ExampleHorizontal(this.books);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -36,16 +42,59 @@ class ExampleHorizontal extends StatelessWidget {
               color = Colors.red;
 
 
-            return  new Container(
-              color: color,
-              width: 20,
-              height: 20,
+            return  new ListView(
+              //CrossAxisAlignment.center,
+              children: <Widget>[
+                /*Scaffold(
+                  /*margin: new EdgeInsets.all(2.0),*/
+                  body: Image.network(
+                      'https://imagessl3.casadellibro.com/a/l/t0/73/9788490628973.jpg',
+                    ),
+                )*/
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.network(
+                    this.books[index].picture,
+                  )
+                ),
+
+                Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        this.books[index].title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                ),
+
+                Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      this.books[index].author,
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                ),
+
+
+              /*Container(
+                color: color,
+                width: 20,
+                height: 20,
+              ),*/
+
+              ],
             );
           },
 
           indicatorLayout: PageIndicatorLayout.COLOR,
           autoplay: true,
-          itemCount: 3,
+          itemCount: books.length,
           pagination: null,
           control: null,
           viewportFraction: 0.8,
--- lib/Pages/discover_page.dart
+++ lib/Pages/discover_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'book_page.dart';
+import 'package:bookifyapp/Models/Book.dart';
 
 class DiscoverPage extends StatelessWidget {
   final Color color;
@@ -11,6 +12,16 @@ class DiscoverPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    List<Book> books =  List();
+    Book book1 = new Book("En los Zapatos de Valeria", "Elisabeth Benavent", "https://imagessl3.casadellibro.com/a/l/t0/73/9788490628973.jpg");
+    Book book2 = new Book("En busca del chico irrompible", "Coque Mesa", "https://imagessl9.casadellibro.com/a/l/t5/59/9788408228059.jpg");
+    Book book3 = new Book("Con el amor bastaba", "Maxim Huerta", "https://imagessl2.casadellibro.com/a/l/t5/92/9788408221692.jpg");
+
+    books.add(book1);
+    books.add(book2);
+    books.add(book3);
+
     return Scaffold(
       body: Container(
         color: color,
@@ -30,7 +41,7 @@ class DiscoverPage extends StatelessWidget {
                   onPressed: () {
                     //Navigator.push(context, BookPage("title"));
                     Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => BookPage("title")));
+                        .push(MaterialPageRoute(builder: (context) => BookPage("title", books)));
                   },
                 ),
 
@@ -69,9 +80,10 @@ class DiscoverPage extends StatelessWidget {
       control: new SwiperControl(),
     );
   }*/
+
   //Use the navigator like you usually do with .of(context) method
-  _openBooksPage(BuildContext context) => Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => BookPage("title")));
+  /*_openBooksPage(BuildContext context) => Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => BookPage("title")));*/
 
   // Exemple: https://github.com/ayham95/Custom-navigator/tree/master/custom_scaffold_example
 }
