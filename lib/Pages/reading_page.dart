01db5c6a8bb0eae683b1e67e0e9f65f84125da44
rspaimoc2 <raduspaimoc@gmail.com>
2020-06-15 19:31:18 +0200

    [WIP] Reading page list pre-design adjustment.

--- lib/Pages/reading_page.dart
+++ lib/Pages/reading_page.dart
@@ -96,16 +96,62 @@ class _ListPageState extends State<ListPage> {
       ),
     );*/
 
-    final makeListTile = ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        leading: Container(
-          padding: EdgeInsets.only(right: 12.0),
-          decoration: new BoxDecoration(
+    final makeListTile = Container(
+      color:  Color.fromRGBO(64, 75, 96, .9),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(right: 12.0),
+              decoration: new BoxDecoration(
               border: new Border(
                   right: new BorderSide(width: 1.0, color: Colors.white24))),
-          child: Icon(Icons.autorenew, color: Colors.white),
+                child: Container(
+                  height: 150,
+                  width: 100,
+                  child: Image.network("https://imagessl3.casadellibro.com/a/l/t0/73/9788490628973.jpg"),
+                ) //Icon(Icons.autorenew, color: Colors.white),
+            ),
+
+            Column(
+              children: <Widget>[
+                Text(
+                  "Introduction to Driving",
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+
+                Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.linear_scale, color: Colors.yellowAccent),
+                    Text(" Intermediate", style: TextStyle(color: Colors.white))
+                  ],
+                ),
+
+              ],
+            ),
+
+            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+          ],
         ),
-        title: Text(
+      )
+    );
+
+    /*ListTile(
+        //contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          //padding: EdgeInsets.only(right: 12.0),
+          /*decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),*/
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.white,
+          ) //Icon(Icons.autorenew, color: Colors.white),
+        ),
+        /*title: Text(
           "Introduction to Driving",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
@@ -118,13 +164,14 @@ class _ListPageState extends State<ListPage> {
           ],
         ),
         trailing:
-        Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+        Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0)*/);*/
 
 
     final makeCard = Card(
       elevation: 8.0,
       margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
       child: Container(
+        height: 160,
         decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
         child: makeListTile,
       ),
