b082b6b69b5c90a7d7f9a055b831ad3459719026
rspaimoc2 <raduspaimoc@gmail.com>
2020-07-01 21:32:38 +0200

    Preview friends widget finished.

--- lib/LayoutWidgets/friends_preview.dart
+++ lib/LayoutWidgets/friends_preview.dart
@@ -6,8 +6,10 @@ import 'package:bookifyapp/Pages/book_page.dart';
 class FriendsPreview extends StatelessWidget {
 
   //CarouselCard({Key key}) : super(key: key);
-  List<User> friends_reading;
-  FriendsPreview(this.friends_reading);
+  final List<User> friendsReading;
+  final String defaultUserUrl = 'https://cdn2.iconfinder.com/data/icons/user-interface-180/128/User-Interface-209-512.png';
+
+  FriendsPreview(this.friendsReading);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +17,14 @@ class FriendsPreview extends StatelessWidget {
       width: 80,
       height: 30,
       //color: Colors.white,
-      child: Stack(
+      child: _getCircleAvatars(),
+    );
+  }
+
+  _getCircleAvatars(){
+    if(friendsReading.length > 2) {
+      var num = friendsReading.length - 2;
+      return  Stack(
           children: <Widget>[
             Align(
               alignment: Alignment.centerLeft,
@@ -23,8 +32,8 @@ class FriendsPreview extends StatelessWidget {
                 backgroundColor: Colors.white,
                 child: CircleAvatar(
                   radius: 18,
-                  backgroundColor: Colors.red,
-                  backgroundImage: NetworkImage('https://avatars3.githubusercontent.com/u/16152037?s=460&u=39193767252f6f4b1d95dd075081a151dca70131&v=4'), // Provide your custom image
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(this.defaultUserUrl), // Provide your custom image
                 ),
               ),
             ),
@@ -34,8 +43,49 @@ class FriendsPreview extends StatelessWidget {
                 backgroundColor: Colors.white,
                 child: CircleAvatar(
                   radius: 18,
-                  backgroundColor: Colors.red,
-                  backgroundImage: NetworkImage('https://avatars0.githubusercontent.com/u/13257788?s=460&u=aaeef51a634fb1db0d1dc7337ec9e4515e9a314f&v=4'), // Provide your custom image
+                  backgroundColor: Colors.white,
+                  backgroundImage:  NetworkImage(this.friendsReading[0].profilePictureUrl),
+                  //backgroundImage: NetworkImage('https://avatars0.githubusercontent.com/u/13257788?s=460&u=aaeef51a634fb1db0d1dc7337ec9e4515e9a314f&v=4'), // Provide your custom image
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  child: Text("+" + num.toString()), // Provide your custom image
+                ),
+              ),
+            ),
+
+          ]
+      );
+    } else if (friendsReading.length == 2) {
+      return  Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.centerLeft,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(this.defaultUserUrl), // Provide your custom image
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  backgroundImage:  NetworkImage(this.friendsReading[0].profilePictureUrl),
+                  //backgroundImage: NetworkImage('https://avatars0.githubusercontent.com/u/13257788?s=460&u=aaeef51a634fb1db0d1dc7337ec9e4515e9a314f&v=4'), // Provide your custom image
                 ),
               ),
             ),
@@ -45,16 +95,56 @@ class FriendsPreview extends StatelessWidget {
                 backgroundColor: Colors.white,
                 child: CircleAvatar(
                   radius: 18,
-                  backgroundColor: Colors.red,
-                  backgroundImage: NetworkImage('https://avatars0.githubusercontent.com/u/35029261?s=460&u=c54ea4c26c7f0659c014f362e538d2927f567a4f&v=4'), // Provide your custom image
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(this.friendsReading[0].profilePictureUrl), // Provide your custom image
                 ),
               ),
             ),
 
           ]
+      );
+    } else if (friendsReading.length == 1) {
+      return  Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.centerLeft,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(this.defaultUserUrl), // Provide your custom image
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(this.defaultUserUrl),
+                  //backgroundImage: NetworkImage('https://avatars0.githubusercontent.com/u/13257788?s=460&u=aaeef51a634fb1db0d1dc7337ec9e4515e9a314f&v=4'), // Provide your custom image
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(this.friendsReading[0].profilePictureUrl), // Provide your custom image
+                ),
+              ),
+            ),
 
-      ),
-    );
+          ]
+      );
+    }
   }
+
 }
 
