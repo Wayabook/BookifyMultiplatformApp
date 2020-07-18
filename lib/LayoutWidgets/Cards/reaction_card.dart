0c9d96ed296fbc01c5b9b31a342ce80c2835f594
rspaimoc2 <raduspaimoc@gmail.com>
2020-07-18 19:33:16 +0200

    [WIP] Reactions stateful widget.

--- lib/LayoutWidgets/Cards/reaction_card.dart
+++ lib/LayoutWidgets/Cards/reaction_card.dart
@@ -2,48 +2,70 @@ import 'package:bookifyapp/Models/Reaction.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ReactionCard extends StatelessWidget {
+class ReactionCard extends StatefulWidget {
   Reaction reaction;
   ReactionCard(this.reaction);
 
   @override
+  _ReactionCard createState() => _ReactionCard();
+}
+
+class _ReactionCard extends State<ReactionCard> {
+  bool tapped = false;
+  String text = "";
+
+  void changeText() {
+    setState(() {
+      if(tapped = false){
+        tapped = true;
+      }
+      text = "99";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    text = widget.reaction.name;
     return Container(
-      height: double.infinity,
-      width: double.infinity,
-      child:  Card(
-          color: Colors.blueGrey,
-          margin: EdgeInsets.all(10),
-          /*shape: RoundedRectangleBorder(
+        height: double.infinity,
+        width: double.infinity,
+        child: GestureDetector(
+          onTap: changeText,
+          child:  Card(
+              color: Colors.blueGrey,
+              margin: EdgeInsets.all(10),
+              /*shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20.0),
         ),*/
-          elevation: 10,
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                this.reaction.image,
-                height: 50,
-                width: 50,
-              ),
+              elevation: 10,
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    widget.reaction.image,
+                    height: 50,
+                    width: 50,
+                  ),
 
-              /*Padding(
+                  /*Padding(
                 padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
               ),*/
 
-              Text(this.reaction.name,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 8,
-                  //fontWeight: FontWeight.bold,
-                ),
-              ),
+                  Text(
+                    "$text",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 8,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
 
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-              ),
-            ],
-          )
-      )
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  ),
+                ],
+              )
+          ),
+        )
     );
   }
 }
