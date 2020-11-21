7ed98594422e185824bc1c42feab7cb87e41a223
raduspaimoc <raduspaimoc@gmail.com>
2020-11-21 14:38:20 +0100

    Shop Tests Created.

---
+++ test/shop_test.dart
@@ -0,0 +1,22 @@
+//import 'package:test/test.dart';
+
+import 'package:bookifyapp/Models/Shop.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  group('Shop Tests', () {
+    test('Get Shop Name', (){
+
+      final shop =Shop.withoutIds("ShopName", "ShopLogoUrl");
+
+      expect(shop.name, "ShopName");
+    });
+
+    test('Get Shop Logo Url', (){
+
+      final shop =Shop.withoutIds("ShopName", "ShopLogoUrl");
+
+      expect(shop.logo_url, "ShopLogoUrl");
+    });
+  });
+}
